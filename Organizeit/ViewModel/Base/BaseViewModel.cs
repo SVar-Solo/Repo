e4985951260af574916f3233aac3753234f24f28
Svarrogg <svarrogg@yandex.ru>
2020-03-26 20:10:45 +0300

    Добавьте файлы проекта.

---
+++ Organizeit/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace Organizeit
+{
+   //[ImplementPropertyChanged]
+   /// <summary>
+   /// Базовая ViewModel посылающая PropertyChanged при необходимости
+   /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Событие откликающееся при измении значения свойства потомка
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Вызвает событие <see cref="PropertyChanged"/>
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Organizeit/ViewModel/Base/RelayCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Organizeit
+{
+    /// <summary>
+    /// Базовая команда для запуска Действия
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+ 
+        #region Private Members
+
+        /// <summary>
+        /// действие для запуска
+        /// </summary>
+        private Action mAction;
+
+
+        #endregion
+
+        #region Public Events
+        /// <summary>
+        /// Событие срабатывающее когда значение <see cref="CanExecute(object)"/> было изменено
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => {};
+        #endregion
+
+        #region Constructor
+        public RelayCommand(Action action)
+        {
+            mAction = action;
+        }
+        #endregion
+
+        #region Command Methods
+
+        /// <summary>
+        /// команда ретрансляции всегда может быть выполнена
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Выполняет действие команды
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            mAction();
+        }
+
+        #endregion
+    }
+}
